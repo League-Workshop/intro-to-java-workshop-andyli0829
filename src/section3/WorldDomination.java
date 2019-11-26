5cf9a5605191e3702455573b41ef93166593cde5
League Student <league@iMac-24.attlocal.net>
2019-11-26 15:27:17 -0800

    day 3 part 1

--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,11 +6,15 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+String friend= JOptionPane.showInputDialog("do you know java?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+if (friend.equalsIgnoreCase("yes")) {
+	JOptionPane.showMessageDialog(null, "they will rule the world");
+}
 		// 3. Otherwise, wish them good luck washing dishes.
-
+else {
+	JOptionPane.showMessageDialog(null, "good luck, you will wishing dishes");
+}
 	}
 }
 
